4b42254cfda89d6bc47e648e3bfdeb9386fd7eff
Pranshu Mishra <noidafam@gmail.com>
2023-04-11 23:08:25 +0530

    .

---
+++ login.js
@@ -0,0 +1,33 @@
+function findl(){
+    if (typeof window !== "undefined") {
+        const username=document.getElementById("user").value;
+        const pass=document.getElementById("password").value;
+        const { MongoClient, ServerApiVersion } = require('mongodb');
+        alert("hi");
+        async function main(){
+            const uri = "mongodb+srv://user1:pass@bsafelogs.xj9kksu.mongodb.net/?retryWrites=true&w=majority";
+            const client = new MongoClient(uri);
+            try {
+                alert("hi");
+                await client.connect();
+                await find(client,username,pass);
+            } catch (e) {
+                console.error(e);
+            } finally {
+                await client.close();
+            }
+        }
+        main().catch(console.error);
+        async function find(client,user,password){
+            const result=await client.db("bsafe").collection("userlogs").findOne({User_id: user,Password: password});
+            if (result){
+                console.log("Data found");
+                alert("Logged in");
+            }
+            else{
+                console.log("No data found");
+                alert("Login failed!");
+            }
+        }
+    }
+}
