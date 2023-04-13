2172120f7e4829430a0fa2746d49313838c29898
Pranshu Mishra <noidafam@gmail.com>
2023-04-13 23:25:19 +0530

    .

--- connect.js
+++ connect.js
@@ -1,9 +1,38 @@
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+const { MongoClient, ServerApiVersion } = require('mongodb');
+async function main() {
+  const uri = "mongodb+srv://user1:pass@bsafelogs.xj9kksu.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    await listDatabases(client);
+    await find(client,"pmishra2k3","pass1234");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+main().catch(console.error);
+async function insert(client,inst){
+  const result=await client.db("bsafe").collection("userlogs").insertOne(inst);
+  if (result){
+    console.log("Data added successfully");
+  }
+  else{
+    console.log("Data could not be added");
+  }
+}
+async function find(client,user,password){
+  const result=await client.db("bsafe").collection("userlogs").findOne({User_id: user,Password: password});
+  if (result){
+    console.log(result);
+  }
+  else{
+    console.log("No data found");
+  }
+}
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` -[${db.name}]`));
+};
--- login.js
+++ login.js
@@ -1,25 +1,38 @@
-var username=document.getElementById("user").value;
-var pass=document.getElementById("password").value;
-const { MongoClient } = require('mongodb');
-async function main(){
-    const uri = "mongodb+srv://user1:pass@bsafelogs.xj9kksu.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
-        await find(client,username,pass);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+const { MongoClient, ServerApiVersion } = require('mongodb');
+async function main() {
+  const uri = "mongodb+srv://user1:pass@bsafelogs.xj9kksu.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    await listDatabases(client);
+    await find(client,"pmishra2k3","pass1234");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 main().catch(console.error);
-async function find(client,user,password){
-    const result=await client.db("bsafe").collection("userlogs").findOne({User_id: user,Password: password});
-    if (result){
-        console.log("Data found");
-    }
-    else{
-        console.log("No data found");
-    }
+async function insert(client,inst){
+  const result=await client.db("bsafe").collection("userlogs").insertOne(inst);
+  if (result){
+    console.log("Data added successfully");
+  }
+  else{
+    console.log("Data could not be added");
+  }
 }
+async function find(client,user,password){
+  const result=await client.db("bsafe").collection("userlogs").findOne({User_id: user,Password: password});
+  if (result){
+    console.log(result);
+  }
+  else{
+    console.log("No data found");
+  }
+}
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` -[${db.name}]`));
+};
